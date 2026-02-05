f101050b8d1298f51715231a5bc18651620f37fa
fatmoney <ogwknt@gmail.com>
2026-02-05 11:15:47 +0900

    a

--- keyboards/sonatina/_/user_keycodes.h
+++ keyboards/sonatina/_/user_keycodes.h
@@ -1,11 +1,18 @@
-#define _ _______
-#define G_ENT MT(MOD_LGUI, KC_ENT)
-#define C_A MT(MOD_LCTL, KC_A)
-#define S_DOT MT(MOD_LSFT, KC_DOT)
-#define S_Z MT(MOD_LSFT, KC_Z)
-#define S_ENT MT(MOD_LSFT, KC_ENT)
-#define L1_SPC LT(1, KC_SPC)
-#define L2_ENT LT(2, KC_ENT)
-#define SS G(S(KC_4)) // Screen Shot (macOS)
-#define EISU KC_LANG2
-#define BACK G(KC_LBRC) // Go back in the browser
+enum {
+    _ = KC_TRANSPARENT,
+
+    G_ENT = MT(MOD_LGUI, KC_ENT),
+
+    C_A = MT(MOD_LCTL, KC_A),
+
+    S_DOT = MT(MOD_LSFT, KC_DOT),
+    S_Z = MT(MOD_LSFT, KC_Z),
+    S_ENT = MT(MOD_LSFT, KC_ENT),
+
+    L1_SPC = LT(1, KC_SPC),
+    L2_ENT = LT(2, KC_ENT),
+
+    SS = G(S(KC_4)), // Screen Shot (macOS)
+    EISU = KC_LNG2,
+    BACK = G(KC_LBRC) // Go back in the browser
+};
